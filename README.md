9f83f3e572df1624e5f8c2126df86bb70a7a6290
Dave <dave.schwantes@gmail.com>
2015-02-15 18:41:28 -0800

    Got seeds set up to get started with basic usage, plus some README updates

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 LawnTent
 ========
 Because I can't resist reinventing the wheel.
+
+# What Is This?
+
+LawnTent is (will be) a content management that produces JSON output rather than an actual website.
+
+# Why Built Another Content Management System?
+
+I often want a web-based way to write posts/content with a customizable template of fields but not be tied to a page generator/theme system like WordPress where I end up fighting the system to get things looking the way I want. For a while I was using WordPress to write content and a library to access the WordPress database directly to render things. This is how [mountsaintawesome.com](http://mountsaintawesome.com) is built but that seemed dumb and I had use a kind of hacky system to associate comics with posts.
+
+Besides, it might be fun to build.
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+unless User.where(email: "admin@lawntent.com").first
+  puts "** Adding Admin User **"
+  user = User.create! :email => 'admin@lawntent.com', :password => 'password', :password_confirmation => 'password'
+  user.toggle!(:admin)
+  puts "(User #{user.email} added with simple password)"
+  puts ""
+end
+
+puts "** Create Simple Blog Post Collection **"
+blog = PostCollection.where(title: "Blog").first_or_create
+
+puts "** Create Simple Blog Post Template **"
+template = PostTemplate.where(title: "Blog Post", post_collection: blog).first_or_create
+title_field = TemplateField.where(post_template: template, label: "title", field_type: "text").first_or_create
+content_field = TemplateField.where(post_template: template, label: "content", field_type: "textblock").first_or_create
