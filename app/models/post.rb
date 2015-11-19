32d78e6bd324d8ee248e96bb798dfac8c1171bb2
Dave <dave.schwantes@gmail.com>
2015-11-18 21:12:23 -0800

    Rebuilt Post#get_content and added a few nice aliases

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,17 @@ class Post < ActiveRecord::Base
   has_one :post_template
   belongs_to :user
   belongs_to :post_collection
+
+  alias_attribute :author, :user
+
+  def get_content
+    additional_fields = {
+      post_id: id,
+      collection_id: post_collection.id,
+      author: author.try(:email),
+      created_at: created_at
+    }
+
+    content.to_h.merge(additional_fields)
+  end
 end
--- app/models/post_collection.rb
+++ app/models/post_collection.rb
@@ -3,6 +3,8 @@ class PostCollection < ActiveRecord::Base
   has_many :posts
   has_many :post_templates
 
+  alias_attribute :name, :title
+
   def template
     post_templates.first
   end
