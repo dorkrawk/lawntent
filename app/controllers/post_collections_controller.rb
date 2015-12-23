f8add3ca843273f5286a3ef7a09829596eca9303
Dave Schwantes <dschwantes@instacart.com>
2015-12-23 15:44:09 -0800

    Updates to post writing interface and some work with the application template

--- app/controllers/post_collections_controller.rb
+++ app/controllers/post_collections_controller.rb
@@ -9,24 +9,25 @@ class PostCollectionsController < ApplicationController
   def new_post
     @user = User.first # for now
     @template = @post_collection.template
-    #@post_fields = @template.template_fields.map { |tf| tf.get_new_field_value }
   end
 
   def create_post
+    # clean this up to a .map?
+    content_json = {}
+    params[:post][:field_values].each do |value_label, value|
+      content_json[value_label.to_sym] = value
+    end
     post = Post.new(post_collection_id: params[:post][:post_collection_id],
                     post_template_id: params[:post][:post_template_id],
-                    user_id: params[:post][:user_id])
-    post.transaction do
-      post.save!
-      params[:field_values].each do |value|
-        template_field = TemplateField.find(value.first.to_i)
-        field_type = template_field.field_type
-        value_field = template_field.value_field.to_sym
-        field_value = value.last
-        new_field = FieldValue.create(post: post, template_field: template_field, field_type: field_type, value_field => field_value)
-      end
+                    user_id: params[:post][:user_id],
+                    content: content_json)
+
+    if post.save
+      flash['notice'] = "Post created"
+      redirect_to post_preview_path(post.post_collection_id, post.id)
+    else
+      flash['error'] = "Error creating post: #{post.errors.full_messages.to_sentence}"
     end
-    redirect_to post_preview_path(post.post_collection_id, post.id)
   end
 
   private
